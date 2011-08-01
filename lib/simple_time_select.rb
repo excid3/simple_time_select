e3b4185f12038c023496ade01f961b76914995ab
Casey Watts <casey.s.watts@gmail.com>
2011-08-01 15:02:46 -0400

    *A default time can be set (especially useful for editing objects) *Cross-midnight capability. Start and end times don’t need to be on the same day. An asterisk is placed after anything in “the next day” [12:30*]. *Start and end times can be any time; it doesn’t just round to the nearest hour. *Start and end times can be excluded from the form if needed.

--- lib/simple_time_select.rb
+++ lib/simple_time_select.rb
@@ -5,91 +5,131 @@ module ActionView::Helpers
         
         # Although this is a datetime select, we only care about the time.  Assume that the date will
         # be set by some other control, and the date represented here will be overriden
-        
-        val_minutes = @datetime.kind_of?(Time) ? @datetime.min + @datetime.hour*60 : @datetime
-        
+                
         @options[:time_separator] = ""
-        
+        @options[:include_start_time] = true  if @options[:include_start_time].nil?
+        @options[:include_end_time] = false   if @options[:include_end_time].nil?
+
+
         # Default is 15 minute intervals
         minute_interval = 15
-        if @options[:minute_interval] 
-          minute_interval = @options[:minute_interval] 
+        if @options[:minute_interval]
+          minute_interval = @options[:minute_interval]
         end
 
-        start_minute = 0
-        # @options[:start_hour] should be specified in military
-        # i.e. 0-23
-        if @options[:start_hour]
-          start_minute = @options[:start_hour] * 60
+
+        if @options[:start_time]
+          start_minute = @options[:start_time].hour * 60 + @options[:start_time].min
+        else
+          start_minute = 0
         end
-        
-        end_minute = 1439
-        # @options[:end_hour] should be specified in military
-        # i.e. 0-23
-        if @options[:end_hour]
-          end_minute = ( @options[:end_hour] + 1 ) * 60 - 1  
+        start_minute = start_minute + 1 unless @options[:include_start_time]
+
+        if @options[:end_time]
+          end_minute = @options[:end_time].hour * 60 + @options[:end_time].min
+          end_minute += 1440    if (@options[:end_time].day == ((@options[:start_time] + 1.day).day))
+        else
+          end_minute = 1439
         end
-        
+        end_minute = end_minute - 1 unless @options[:include_end_time]
+
+
+
+
+        if @options[:default]
+          val_minutes = @options[:default].min + @options[:default].hour*60
+          #if start/end time is specified, they might be on different days
+          if  @options[:start_time] && @options[:end_time]
+            if (@options[:end_time].to_date == (@options[:start_time].to_date + 1.day)) && (val_minutes < start_minute)
+              val_minutes += 1440
+            end
+          end
+          # Even if :default isn't a valid multiple of 15, it should still be used
+          # This condition selects the earliest valid minute after :default to be the default
+          unless (@options[:default].min % minute_interval) == 0
+            val_minutes = val_minutes - (@options[:default].min % minute_interval) + minute_interval
+          end
+        end
+
+
         if @options[:use_hidden] || @options[:discard_minute]
           build_hidden(:minute, val)
         else
           minute_options = []
           start_minute.upto(end_minute) do |minute|
-            if minute%minute_interval == 0
-              ampm = minute < 720 ? ' AM' : ' PM'
-              hour = minute/60
-              minute_padded = zero_pad_num(minute%60)
-              hour_padded = zero_pad_num(hour)
-              ampm_hour = ampm_hour(hour)
-              
-              val = "#{hour_padded}:#{minute_padded}:00"
-              minute_options << ((val_minutes == minute) ? 
-                %(<option value="#{val}" selected="selected">#{ampm_hour}:#{minute_padded}#{ampm}</option>\n) :
-                %(<option value="#{val}">#{ampm_hour}:#{minute_padded}#{ampm}</option>\n)
-              )
+
+            if minute < 1440
+              if minute%minute_interval == 0
+                ampm = minute < 720 ? ' AM' : ' PM'
+                hour = minute/60
+                minute_padded = zero_pad_num(minute%60)
+                hour_padded = zero_pad_num(hour)
+                ampm_hour = ampm_hour(hour)
+                val = "#{hour_padded}:#{minute_padded}:00"
+                minute_options << ((val_minutes == minute) ?
+                  %(<option value="#{val}" selected="selected">#{ampm_hour}:#{minute_padded}#{ampm}</option>\n) :
+                  %(<option value="#{val}">#{ampm_hour}:#{minute_padded}#{ampm}</option>\n)
+                )
+              end
+            elsif minute < 2880
+              minute -= 1440
+              if minute%minute_interval == 0
+                ampm = minute < 720 ? ' AM' : ' PM'
+                hour = minute/60
+                minute_padded = zero_pad_num(minute%60)
+                hour_padded = zero_pad_num(hour)
+                ampm_hour = ampm_hour(hour)
+                val = "#{hour_padded}:#{minute_padded}:00+"
+                minute_options << ((val_minutes == minute + 1440) ?
+                  %(<option value="#{val}" selected="selected">#{ampm_hour}:#{minute_padded}#{ampm}*</option>\n) :
+                  %(<option value="#{val}">#{ampm_hour}:#{minute_padded}#{ampm}*</option>\n)
+                )
+
+              end
             end
+
           end
           build_select(:minute, minute_options.join(' '))
         end
       end
       alias_method_chain :select_minute, :simple_time_select
-      
-      
+
+
       def select_hour_with_simple_time_select
         return select_hour_without_simple_time_select unless @options[:simple_time_select].eql? true
         # Don't build the hour select
         #build_hidden(:hour, val)
       end
       alias_method_chain :select_hour, :simple_time_select
-      
+
       def select_second_with_simple_time_select
         return select_second_without_simple_time_select unless @options[:simple_time_select].eql? true
         # Don't build the seconds select
         #build_hidden(:second, val)
       end
       alias_method_chain :select_second, :simple_time_select
-      
+
       def select_year_with_simple_time_select
         return select_year_without_simple_time_select unless @options[:simple_time_select].eql? true
         # Don't build the year select
         #build_hidden(:year, val)
       end
       alias_method_chain :select_year, :simple_time_select
-      
+
       def select_month_with_simple_time_select
         return select_month_without_simple_time_select unless @options[:simple_time_select].eql? true
         # Don't build the month select
         #build_hidden(:month, val)
       end
       alias_method_chain :select_month, :simple_time_select
-      
+
       def select_day_with_simple_time_select
         return select_day_without_simple_time_select unless @options[:simple_time_select].eql? true
         # Don't build the day select
         #build_hidden(:day, val)
       end
       alias_method_chain :select_day, :simple_time_select
-     
+
   end
 end
 
